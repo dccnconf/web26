4f62bd648eea8dd81308b23a081697f00cb6e566
aleksandrsokolov <timac11@bk.ru>
2026-06-18 09:03:16 +0200

    updates

---
+++ data/plenary/jayakody.md
@@ -0,0 +1,10 @@
+---
+title: "A New Green Paradigm for 5G and Beyond Communication Networks"
+---
+
+The rapid growth of connected devices, data-intensive applications, and demand for higher data rates is reshaping the evolution of cellular mobile communications. As 5G and beyond networks aim to support massive connectivity, ultra-reliable services, and energy-demanding applications, energy-efficient communication has become a critical requirement.
+
+This talk presents a green communication paradigm based on wireless energy harvesting, with particular emphasis on radio frequency (RF)-enabled simultaneous wireless information and power transfer (SWIPT). By enabling RF signals to carry both information and energy, SWIPT offers a reliable and predictable approach to powering wireless devices while reducing dependence on external energy sources.
+
+The talk will provide a high-level overview of wireless energy harvesting techniques, recent advances in SWIPT, current state-of-the-art developments, and emerging research directions. It will also discuss how RF energy harvesting intersects with semantic communications, quantum communications, and other 5G and beyond technologies, highlighting new opportunities for sustainable, intelligent, and energy-efficient wireless networks.
+
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,6 +17,8 @@ import TpcMembersList from "../components/TpcMembersList";
 import {getAllTpcMembers} from "../libs/tpc";
 import KeynoteSpeakers from "../components/KeynoteSpeakers";
 import {getAllSpeakers} from "../libs/keynotes";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArchive} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({committeeMembers, organizations, topics, deadlines, tpcMembers, fees, speakers}) {
   return (
@@ -72,17 +74,17 @@ export default function Home({committeeMembers, organizations, topics, deadlines
       </section>
 
       <section id="fees" className="pt-12 bg-purple-100">
-        <div className="container mx-auto px-4 pb-12 lg:w-1/2">
+        <p className="container mx-auto px-4 pb-12 lg:w-1/2">
           <h2 className="h2">Registration & Fees</h2>
           <Fees className="mt-8"
-              fees={fees}
+                fees={fees}
           />
-          {/*<p className="text-xl">*/}
-          {/*  <Link href={"/support"} as="/support#payment">*/}
-          {/*    <a className="text-blue-500 hover:underline">Read more...</a>*/}
-          {/*  </Link>*/}
-          {/*</p>*/}
-        </div>
+          <p className="text-xl">
+            <a href="/downloads/payments_instruction.docx" className="text-blue-500 hover:underline">
+              Read more
+            </a>
+          </p>
+        </p>
       </section>
 
       <section id="committee" className="pt-12">
